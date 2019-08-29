d5be7bed07d452427f50071875a369b09b50edd2
nemanjamil <nemanjamil@gmail.com>
2019-08-29 16:42:54 +0200

    Test TODO

--- dockerHub/run.sh
+++ dockerHub/run.sh
@@ -3,4 +3,7 @@ echo "BIN is initiated"
 php artisan key:generate
 php artisan config:clear
 # php artisan migrate --seed
+
+
+# php artisan make:auth
 #php artisan serve --host=0.0.0.0 --port=$APP_PORT#!/usr/bin/env bash
--- laravelData/routes/api.php
+++ laravelData/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('todo', 'TodoController@index');
+Route::group(['middleware' => 'auth:api'], function() {
+});
+
+Route::group(['middleware' => ['web']], function () {
+});
