27ae3c244f45bb3d054063d0f73c2797c700d570
nemanjamil <nemanjamil@gmail.com>
2019-09-12 16:55:18 +0200

    Fix HTML strip tags and html_entity_decode

--- laravelData/app/Http/Controllers/SqmsExamVersionController.php
+++ laravelData/app/Http/Controllers/SqmsExamVersionController.php
@@ -252,7 +252,8 @@ class SqmsExamVersionController extends Controller
     protected function saveToStorage($savedata, $json, $xml, $hash_salt)
     {
         if ($savedata == 'download') {
-            $namefile = str_replace("-", "", $json["Exam_Id"]);
+            //$namefile = str_replace("_", "", $json["ExamVersion_Name"]);
+            $namefile = $json["ExamVersion_Name"];
             $publiclink = 'public/' . $namefile;
             Storage::makeDirectory($publiclink);
             Storage::put($publiclink . '/' . $namefile . '.json', json_encode($json));
@@ -370,7 +371,7 @@ class SqmsExamVersionController extends Controller
         foreach ($numberOfQuestionTotalExam as $k => $v) {
             $sprint_sqms_question_id = sprintf("%010d", $v->sqms_question_id);
             $qarr['question_id'] = $sprint_sqms_question_id;
-            $qarr['question_text'] = $v->question;
+            $qarr['question_text'] = strip_tags(html_entity_decode($v->question, ENT_COMPAT | ENT_HTML401, 'UTF-8'));
             //$qarr['sqms_exam_version_id'] = $v->sqms_exam_version_id;
             $qarr['answers'] = [];
             $qarr['answersSelected'] = [];
@@ -390,7 +391,7 @@ class SqmsExamVersionController extends Controller
                     $correct_answ = $v->correct;
                     $answer_is_sprint = sprintf("%010d", $v->sqms_answer_id);
                     $forls['answer_id'] = $answer_is_sprint;
-                    $forls['answer_text'] = $v->answer;
+                    $forls['answer_text'] = strip_tags(html_entity_decode($v->answer, ENT_COMPAT | ENT_HTML401, 'UTF-8'));
                     $forls['correct'] = $correct_answ;
 
                     if ($correct_answ == 1) {
