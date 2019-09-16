5f431bfe3a015fc3fe977268f938e0aac6e14e23
nemanjamil <nemanjamil@gmail.com>
2019-09-16 20:47:51 +0200

    Remove code from initial start

--- dockerHub/app_run.sh
+++ dockerHub/app_run.sh
@@ -3,6 +3,6 @@
 echo "ARTISAN KEY AND CONFIG"
 pwd
 cd /var/www/be
-php artisan key:generate
-php artisan config:clear
-php artisan storage:link
+#php artisan key:generate
+#php artisan config:clear
+#php artisan storage:link
